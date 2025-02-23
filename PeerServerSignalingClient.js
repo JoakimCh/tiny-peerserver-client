2c268f4d36fd78dbfe10cbb0578d072d5bf5af58
JoakimCh <5984627+JoakimCh@users.noreply.github.com>
2025-02-23 16:23:20 +0100

    reduce server list

--- PeerServerSignalingClient.js
+++ PeerServerSignalingClient.js
@@ -7,9 +7,9 @@ export const peerjsIceConfig = {
       urls: [
         'stun:stun.l.google.com:19302',
         'stun:stun1.l.google.com:19302',
-        'stun:stun2.l.google.com:19302',
-        'stun:stun3.l.google.com:19302',
-        'stun:stun4.l.google.com:19302',
+        // 'stun:stun2.l.google.com:19302',
+        // 'stun:stun3.l.google.com:19302',
+        // 'stun:stun4.l.google.com:19302',
       ]
     }, { // from the PeerJS project: https://github.com/peers/peerjs/blob/master/lib/util.ts
       username: 'peerjs',
